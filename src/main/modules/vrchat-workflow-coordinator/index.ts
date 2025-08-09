4592b4065cbd1d24c86f3012c100b76b055197ba
Coooookies <admin@mitay.net>
2025-08-09 09:57:14 +0800

    feat: add Label component

---
+++ src/main/modules/vrchat-workflow-coordinator/index.ts
@@ -0,0 +1,12 @@
+import { createLogger } from '@main/logger'
+import { Module } from '@shared/module-constructor'
+
+export class VRChatAPI extends Module {
+  private readonly logger = createLogger(this.moduleId)
+
+  protected async onInit(): Promise<void> {
+
+  }
+
+  
+}
