d2b672405ef9c435cee210a4f48d07ca3e5f14fe
Coooookies <admin@mitay.net>
2025-08-09 18:11:28 +0800

    feat: implement VRChatWorkflowCoordinator module

---
+++ src/renderer/shared/modules/vrchat-workflow-coordinator.ts
@@ -0,0 +1,17 @@
+import { Dependency, Module } from '@shared/module-constructor'
+import type { MobxRenderer } from './mobx-renderer'
+import type { WorkflowCoordinatorSharedState } from '@shared/types/mobx-shared'
+
+export class VRChatWorkflowCoordinator extends Module {
+  @Dependency('MobxRenderer') declare private mobx: MobxRenderer
+
+  private $!: WorkflowCoordinatorSharedState
+
+  protected onInit(): void {
+    this.$ = this.mobx.use<WorkflowCoordinatorSharedState>(this.moduleId)
+  }
+
+  public get state() {
+    return this.$
+  }
+}
