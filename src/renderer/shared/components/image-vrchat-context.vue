981d71252e31fad661ead687672ceae14128a38e
Coooookies <admin@mitay.net>
2025-08-10 16:13:44 +0800

    feat: proxy VRChat image assets

---
+++ src/renderer/shared/components/image-vrchat-context.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ImageContext } from './ui/image'
+import type { ImageContextProps } from './ui/image'
+
+const props = withDefaults(
+  defineProps<
+    Omit<ImageContextProps, 'src'> & {
+      fileId: string
+      version: number
+      size?: number
+    }
+  >(),
+  {
+    size: 64
+  }
+)
+const imageUrl = props.fileId
+  ? `vrcxnext://vrchat-proxy/image/${props.fileId}/${props.version}/${props.size}`
+  : ``
+</script>
+
+<template>
+  <ImageContext v-bind="props" :src="imageUrl" />
+</template>
