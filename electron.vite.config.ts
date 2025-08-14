08b1cc3ff797ca82a3fd4c070a80450599ffae6e
Coooookies <admin@mitay.net>
2025-08-15 00:06:44 +0800

    feat: add rollup-plugin-visualizer for build analysis and update .gitignore

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,5 +1,6 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin, swcPlugin } from 'electron-vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
 import tailwindcss from '@tailwindcss/vite'
@@ -34,12 +35,36 @@ export default defineConfig({
         '@shared': resolve('src/shared')
       }
     },
-    plugins: [vue(), svgLoader(), tailwindcss()],
+    plugins: [
+      vue(),
+      svgLoader(),
+      tailwindcss(),
+      visualizer({
+        open: true,
+        gzipSize: true,
+        brotliSize: true
+      })
+    ],
     build: {
       minify,
       rollupOptions: {
         input: {
           'main-window': resolve(__dirname, 'src/renderer/main.html')
+        },
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              if (id.includes('reka-ui')) return 'vendor-reka-ui'
+              if (id.includes('motion-v')) return 'vendor-motion-v'
+              if (id.includes('vue-sonner')) return 'vendor-vue-sonner'
+              if (id.includes('vue-i18n')) return 'vendor-vue-i18n'
+              if (id.includes('vee-validate')) return 'vendor-vee-validate'
+              if (id.includes('lodash')) return 'vendor-lodash'
+              if (id.includes('zod')) return 'vendor-zod'
+            }
+
+            return
+          }
         }
       }
     }
