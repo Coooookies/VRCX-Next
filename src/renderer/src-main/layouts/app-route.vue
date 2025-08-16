1b4b877cff4282c08a90283eb30f3ffa57dd3862
Coooookies <admin@mitay.net>
2025-08-17 00:24:22 +0800

    feat: Route page layout

---
+++ src/renderer/src-main/layouts/app-route.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import ScrollContainer from '@renderer/shared/components/scroll-container.vue'
+import { inject } from 'vue'
+import type { Ref } from 'vue'
+import type { ScrollContainerProps } from '@renderer/shared/components/scroll-container.vue'
+
+const props = defineProps<ScrollContainerProps>()
+const leftRoutePadding = inject<Ref<number>>('leftRoutePadding')!
+const rightRoutePadding = inject<Ref<number>>('rightRoutePadding')!
+</script>
+
+<template>
+  <ScrollContainer
+    v-bind="props"
+    class="w-full h-full"
+    :scrollbar-offset-right="4 + rightRoutePadding"
+  >
+    <div
+      class="relative w-full box-border"
+      :style="{
+        paddingLeft: `${leftRoutePadding}px`,
+        paddingRight: `${rightRoutePadding}px`
+      }"
+    >
+      <slot />
+    </div>
+  </ScrollContainer>
+</template>
