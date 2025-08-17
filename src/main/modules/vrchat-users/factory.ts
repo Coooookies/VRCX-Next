dd8efcc9f6cc0289a0dee632e319702d627f4c2c
Coooookies <admin@mitay.net>
2025-08-17 22:00:20 +0800

    feat: Add user information processing capabilities, including the conversion of user trust

---
+++ src/main/modules/vrchat-users/factory.ts
@@ -0,0 +1,68 @@
+import { parseFileUrl, getProfileIconUrl } from '../vrchat-files/parser'
+import { UserTrustRank, UserLanguage } from '@shared/definition/vrchat-users'
+import type { UserAvatar } from '@shared/definition/vrchat-avatars'
+import type { CurrentUser } from '@shared/definition/vrchat-api-response'
+import type { UserInformation } from '@shared/definition/vrchat-users'
+
+export function toCurrentUserInformation(user: CurrentUser): UserInformation {
+  const profileIconUrl = getProfileIconUrl(user)
+  const profileIconFileInfo = parseFileUrl(profileIconUrl)
+  const profileBackgroundFileInfo = parseFileUrl(
+    user.profilePicOverride || user.currentAvatarImageUrl
+  )
+
+  const languages = toUserLanguageTags(user.tags)
+  const trustRank = toUserTrustRank(user.tags)
+  const isSupporter = checkSupporter(user.tags)
+  const avatarFileInfo = parseFileUrl(user.currentAvatarImageUrl)
+  const avatarId = user.currentAvatar
+  const avatar: UserAvatar = {
+    avatarId,
+    allowCopying: user.allowAvatarCopying,
+    imageFileId: avatarFileInfo.fileId,
+    imageFileVersion: avatarFileInfo.version
+  }
+
+  return {
+    userId: user.id,
+    displayName: user.displayName,
+    developerType: user.developerType,
+    status: user.status,
+    statusDescription: user.statusDescription,
+    statusHistory: user.statusHistory,
+    profileIconFileId: profileIconFileInfo.fileId,
+    profileIconFileVersion: profileIconFileInfo.version,
+    profileBackgroundFileId: profileBackgroundFileInfo.fileId,
+    profileBackgroundFileVersion: profileBackgroundFileInfo.version,
+    pronouns: user.pronouns,
+    avatar,
+    bio: user.bio || '',
+    bioLinks: user.bioLinks || [],
+    tags: user.tags,
+    trustRank,
+    platform: user.last_platform,
+    languages,
+    ageVerified: user.ageVerified,
+    ageVerificationStatus: user.ageVerificationStatus,
+    isSupporter
+  }
+}
+
+export function toUserLanguageTags(tags: string[]): UserLanguage[] {
+  const languages = Object.values(UserLanguage)
+  return tags.filter((tag) => languages.includes(tag as UserLanguage)) as UserLanguage[]
+}
+
+export function toUserTrustRank(tags: string[]): UserTrustRank {
+  if (tags.includes(UserTrustRank.Moderator)) return UserTrustRank.Moderator
+  if (tags.includes(UserTrustRank.Trusted)) return UserTrustRank.Trusted
+  if (tags.includes(UserTrustRank.Known)) return UserTrustRank.Known
+  if (tags.includes(UserTrustRank.User)) return UserTrustRank.User
+  if (tags.includes(UserTrustRank.New)) return UserTrustRank.New
+  if (tags.includes(UserTrustRank.Troll)) return UserTrustRank.Troll
+  return UserTrustRank.User
+}
+
+export function checkSupporter(tags: string[]): boolean {
+  return tags.includes('system_supporter')
+}
