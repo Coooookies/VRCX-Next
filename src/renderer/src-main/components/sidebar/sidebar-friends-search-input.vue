f092a6a4f70a2e8b0d91083d122ab2fb5c7d6cf4
Coooookies <admin@mitay.net>
2025-08-20 22:59:18 +0800

    feat: enhance sidebar search input with clear button and improved visibility

--- src/renderer/src-main/components/sidebar/sidebar-friends-search-input.vue
+++ src/renderer/src-main/components/sidebar/sidebar-friends-search-input.vue
@@ -1,20 +1,48 @@
 <script setup lang="ts">
-import { Input } from '@renderer/shared/components/ui/input'
 import { cn } from '@renderer/shared/utils/style'
+import { SearchIcon, XIcon } from 'lucide-vue-next'
+import { Button } from '@renderer/shared/components/ui/button'
+import { Input } from '@renderer/shared/components/ui/input'
+
+const value = defineModel<string>('modelValue', {
+  default: ''
+})
 </script>
 
 <template>
   <div
     :class="
       cn(
+        'relative invisible',
         'opacity-0 transition-all duration-200 ease-[cubic-bezier(.16,1,.3,1)]',
-        'group-has-[*]/sidebar-expanded:opacity-100 group-hover/sidebar-collapsed:opacity-100'
+        'group-has-[*]/sidebar-expanded:opacity-100 group-hover/sidebar-collapsed:opacity-100',
+        'group-has-[*]/sidebar-expanded:visible group-hover/sidebar-collapsed:visible'
       )
     "
   >
     <Input
-      class="relative w-full h-9.5 border-transparent dark:bg-input/55 placeholder:text-sidebar-foreground/35"
+      v-model="value"
       placeholder="Search"
-    ></Input>
+      class="relative w-full h-9.5 pr-9 border-transparent dark:bg-input/55 placeholder:text-sidebar-foreground/35"
+    />
+    <div
+      :class="
+        cn(
+          'absolute right-2 top-1/2 -translate-y-1/2 size-6',
+          'flex items-center justify-center pointer-events-none'
+        )
+      "
+    >
+      <SearchIcon v-if="value.length === 0" class="size-3.5 text-muted-foreground" />
+      <Button
+        v-else
+        size="icon"
+        variant="ghost"
+        class="size-full rounded-sm pointer-events-auto"
+        @click="value = ''"
+      >
+        <XIcon class="size-4" />
+      </Button>
+    </div>
   </div>
 </template>
--- src/renderer/src-main/components/sidebar/sidebar-icon-button.vue
+++ src/renderer/src-main/components/sidebar/sidebar-icon-button.vue
@@ -28,7 +28,7 @@ const props = withDefaults(
           props.variant === 'secondary'
             ? props.active
               ? '!bg-sidebar-foreground/10 dark:!bg-secondary/50'
-              : 'bg-sidebar-foreground/5 dark:bg-input/40 hover:bg-sidebar-foreground/10 hover:dark:bg-input/50 active:bg-sidebar-foreground/5 active:dark:!bg-input/30'
+              : 'bg-sidebar-foreground/5 dark:bg-input/55 hover:bg-sidebar-foreground/10 hover:dark:bg-input/80 active:bg-sidebar-foreground/5 active:dark:!bg-input/40'
             : ''
         )
       "
