f948fa09d761474380cb24476c365fe0fb19dec2
Coooookies <admin@mitay.net>
2025-08-23 01:45:33 +0800

    feat: Enhance Response API Type definition and PipeLine Event Type definition

--- src/shared/definition/vrchat-pipeline.ts
+++ src/shared/definition/vrchat-pipeline.ts
@@ -1,4 +1,14 @@
-import type { CurrentUser, Platform, User, World } from './vrchat-api-response'
+import type {
+  CurrentUser,
+  GroupLimitedMember,
+  GroupRole,
+  InventoryItemType,
+  Notification,
+  NotificationV2,
+  Platform,
+  User,
+  World
+} from './vrchat-api-response'
 
 export interface PipelineUserPlatformHistory {
   platform: Platform
@@ -74,9 +84,103 @@ export interface PipelineEventUserUpdate {
 export interface PipelineEventUserLocation {
   userId: string
   user: User
-  location: string //'traveling:traveling'
+  location: string
   instance: string
   travelingToLocation: string
+}
+
+export interface PipelineEventUserBadgeAssigned {
+  badge: string
+}
+
+export interface PipelineEventUserBadgeUnassigned {
+  badgeId: string
+}
+
+export const PipelineEventContentRefreshContentType = {
+  Gallery: 'gallery',
+  Icon: 'icon',
+  Emoji: 'emoji',
+  World: 'world',
+  Avatar: 'avatar'
+}
+
+export type PipelineEventContentRefreshContentType =
+  (typeof PipelineEventContentRefreshContentType)[keyof typeof PipelineEventContentRefreshContentType]
+
+export const PipelineEventContentRefreshActionType = {
+  Created: 'created',
+  Updated: 'updated',
+  Deleted: 'deleted'
+}
+
+export type PipelineEventContentRefreshActionType =
+  (typeof PipelineEventContentRefreshActionType)[keyof typeof PipelineEventContentRefreshActionType]
+
+export interface PipelineEventContentRefresh {
+  contentType: PipelineEventContentRefreshContentType
+  fileId: string
+  itemId: string
+  itemType: InventoryItemType
+  actionType: PipelineEventContentRefreshActionType
+}
+
+export interface PipelineEventModifiedImageUpdate {
+  fileId: string
+  pixelSize: number
+  versionNumber: number
+  needsProcessing: boolean
+}
+
+export interface PipelineEventInstanceQueueJoined {
+  instanceLocation: string
+  position: number
+}
+
+export interface PipelineEventInstanceQueueReady {
+  instanceLocation: string
+  expiry: string
+}
+
+export interface PipelineEventGroupJoined {
+  groupId: string
+}
+
+export interface PipelineEventGroupLeft {
+  groupId: string
+}
+
+export interface PipelineEventGroupMemberUpdated {
+  member: GroupLimitedMember
+}
+
+export interface PipelineEventGroupRoleUpdated {
+  role: GroupRole
+}
+
+export type PipelineEventNotification = Notification
+
+export interface PipelineEventResponseNotification {
+  notificationId: string
+  receiverId: string
+  responseId: string
+}
+
+export type PipelineEventSeeNotification = string
+export type PipelineEventHideNotification = string
+export type PipelineEventClearNotification = never
+
+export type PipelineEventNotificationV2 = NotificationV2
+
+export interface PipelineEventNotificationV2Update {
+  id: string
+  version: number
+  updates: Partial<NotificationV2>
+}
+
+export interface PipelineEventNotificationV2Delete {
+  ids: string[]
+  version: number
 }
 
 export const PipelineEvents = {
@@ -87,6 +191,7 @@ export const PipelineEvents = {
   ClearNotification: 'clear-notification',
   NotificationV2: 'notification-v2',
   NotificationV2Update: 'notification-v2-update',
+  NotificationV2Delete: 'notification-v2-delete',
   FriendAdd: 'friend-add',
   FriendDelete: 'friend-delete',
   FriendOnline: 'friend-online',
@@ -111,13 +216,14 @@ export const PipelineEvents = {
 export type PipelineEvents = (typeof PipelineEvents)[keyof typeof PipelineEvents]
 
 export type PipelineEventDefinition = {
-  [PipelineEvents.Notification]: {}
-  [PipelineEvents.ResponseNotification]: {}
-  [PipelineEvents.SeeNotification]: {}
-  [PipelineEvents.HideNotification]: {}
-  [PipelineEvents.ClearNotification]: {}
-  [PipelineEvents.NotificationV2]: {}
-  [PipelineEvents.NotificationV2Update]: {}
+  [PipelineEvents.Notification]: PipelineEventNotification
+  [PipelineEvents.ResponseNotification]: PipelineEventResponseNotification
+  [PipelineEvents.SeeNotification]: PipelineEventSeeNotification
+  [PipelineEvents.HideNotification]: PipelineEventHideNotification
+  [PipelineEvents.ClearNotification]: PipelineEventClearNotification
+  [PipelineEvents.NotificationV2]: PipelineEventNotificationV2
+  [PipelineEvents.NotificationV2Update]: PipelineEventNotificationV2Update
+  [PipelineEvents.NotificationV2Delete]: PipelineEventNotificationV2Delete
   [PipelineEvents.FriendOffline]: PipelineEventFriendOffline
   [PipelineEvents.FriendOnline]: PipelineEventFriendOnline
   [PipelineEvents.FriendLocation]: PipelineEventFriendLocation
@@ -127,16 +233,16 @@ export type PipelineEventDefinition = {
   [PipelineEvents.FriendActive]: PipelineEventFriendActive
   [PipelineEvents.UserUpdate]: PipelineEventUserUpdate
   [PipelineEvents.UserLocation]: PipelineEventUserLocation
-  [PipelineEvents.UserBadgeAssigned]: {}
-  [PipelineEvents.UserBadgeUnassigned]: {}
-  [PipelineEvents.ContentRefresh]: {}
-  [PipelineEvents.ModifiedImageUpdate]: {}
-  [PipelineEvents.InstanceQueueJoined]: {}
-  [PipelineEvents.InstanceQueueReady]: {}
-  [PipelineEvents.GroupJoined]: {}
-  [PipelineEvents.GroupLeft]: {}
-  [PipelineEvents.GroupMemberUpdated]: {}
-  [PipelineEvents.GroupRoleUpdated]: {}
+  [PipelineEvents.UserBadgeAssigned]: PipelineEventUserBadgeAssigned
+  [PipelineEvents.UserBadgeUnassigned]: PipelineEventUserBadgeUnassigned
+  [PipelineEvents.ContentRefresh]: PipelineEventContentRefresh
+  [PipelineEvents.ModifiedImageUpdate]: PipelineEventModifiedImageUpdate
+  [PipelineEvents.InstanceQueueJoined]: PipelineEventInstanceQueueJoined
+  [PipelineEvents.InstanceQueueReady]: PipelineEventInstanceQueueReady
+  [PipelineEvents.GroupJoined]: PipelineEventGroupJoined
+  [PipelineEvents.GroupLeft]: PipelineEventGroupLeft
+  [PipelineEvents.GroupMemberUpdated]: PipelineEventGroupMemberUpdated
+  [PipelineEvents.GroupRoleUpdated]: PipelineEventGroupRoleUpdated
 }
 
 export type PipelineEventMessage = {
