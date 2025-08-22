88728bfa206c9b48fc9f3cc9d8d049e09d2a2f63
Coooookies <admin@mitay.net>
2025-08-22 18:55:57 +0800

    feat: add badge area component and integrate location display in sidebar

---
+++ src/renderer/src-main/components/badge/badge-area.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import AreaJP from '@shared/assets/vector/area-jp.svg?component'
+import AreaUSA from '@shared/assets/vector/area-usa.svg?component'
+import AreaEU from '@shared/assets/vector/area-eu.svg?component'
+import AreaUnknown from '@shared/assets/vector/area-unknown.svg?component'
+import { Region } from '@shared/definition/vrchat-api-response'
+import { computed } from 'vue'
+
+const props = defineProps<{
+  region?: Region
+}>()
+
+const locationArea = computed(() => {
+  if (!props.region) {
+    return AreaUnknown
+  }
+
+  switch (props.region) {
+    case Region.Jp:
+      return AreaJP
+    case Region.Us:
+    case Region.Use:
+    case Region.Usw:
+    case Region.Usx:
+      return AreaUSA
+    case Region.Eu:
+      return AreaEU
+    default:
+      return AreaUnknown
+  }
+})
+</script>
+
+<template>
+  <component :is="locationArea" />
+</template>
--- src/renderer/src-main/components/sidebar/sidebar-profile-status-location.vue
+++ src/renderer/src-main/components/sidebar/sidebar-profile-status-location.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
 import ShinyText from '@renderer/shared/components/shiny-text.vue'
-import AreaJP from '@shared/assets/vector/area-jp.svg?component'
-import AreaUSA from '@shared/assets/vector/area-usa.svg?component'
-import AreaEU from '@shared/assets/vector/area-eu.svg?component'
-import AreaUnknown from '@shared/assets/vector/area-unknown.svg?component'
 import { computed } from 'vue'
 import { cn } from '@renderer/shared/utils/style'
 import { useI18n } from '@renderer/shared/locale'
 import { getLocationLabel } from '@renderer/src-main/composables/sidebar-friends'
-import { Region } from '@shared/definition/vrchat-api-response'
 import type { UserLocation } from '@shared/definition/vrchat-users'
+import BadgeArea from '../badge/badge-area.vue'
 
 const { t } = useI18n()
 
@@ -19,28 +15,7 @@ const props = defineProps<{
 }>()
 
 const locationText = computed(() => {
-  return props.location ? getLocationLabel(props.location) : ''
-})
-
-const locationArea = computed(() => {
-  const location = props.location
-  if (!location) {
-    return AreaUnknown
-  }
-
-  switch (location.region) {
-    case Region.Jp:
-      return AreaJP
-    case Region.Us:
-    case Region.Use:
-    case Region.Usw:
-    case Region.Usx:
-      return AreaUSA
-    case Region.Eu:
-      return AreaEU
-    default:
-      return AreaUnknown
-  }
+  return props.location ? getLocationLabel(props.location)(t) : ''
 })
 </script>
 
@@ -57,14 +32,17 @@ const locationArea = computed(() => {
       <span>{{ t('profile.instance_type.private') }}</span>
     </template>
     <template v-else-if="props.isTraveling">
-      <div class="size-4 flex items-center justify-center">
-        <component :is="locationArea" class="size-3.5 animate-[animation-icon-shiny_2s_infinite]" />
+      <div class="size-4 shrink-0flex items-center justify-center">
+        <BadgeArea
+          :region="props.location.region"
+          class="size-3.5 animate-[animation-icon-shiny_2s_infinite]"
+        />
       </div>
       <ShinyText class="truncate" :text="locationText" :speed="2" />
     </template>
     <template v-else>
-      <div class="size-4 flex items-center justify-center">
-        <component :is="locationArea" class="size-3.5" />
+      <div class="size-4 shirnk-0 flex items-center justify-center">
+        <BadgeArea :region="props.location.region" class="size-3.5" />
       </div>
       <span class="truncate">{{ locationText }}</span>
     </template>
