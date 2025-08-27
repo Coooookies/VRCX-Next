ac68e03c5a9b7b23d43242ab85d1ef991c7aba4e
Coooookies <admin@mitay.net>
2025-08-27 23:07:12 +0800

    feat: update styling and dimensions in SidebarNotificationPopoverCard component

--- src/renderer/src-main/components/sidebar/sidebar-notification-popover-card.vue
+++ src/renderer/src-main/components/sidebar/sidebar-notification-popover-card.vue
@@ -12,10 +12,10 @@ import { PopoverContent } from '@renderer/shared/components/ui/popover'
     side="left"
     update-position-strategy="optimized"
     disable-update-on-layout-shift
-    class="relative w-100 h-144 p-0 rounded-lg"
+    class="relative w-94 h-160 p-0 rounded-lg"
   >
-    <div class="pt-4.5 px-5 pb-2">
-      <h2 class="text-lg font-bold">Notifications</h2>
+    <div class="pt-4 px-5 pb-1.5">
+      <h2 class="text-base font-semibold">Notifications</h2>
     </div>
     <Tabs default-value="tab-1" class="w-full items-center">
       <TabsList
@@ -23,9 +23,9 @@ import { PopoverContent } from '@renderer/shared/components/ui/popover'
       >
         <TabsTrigger
           value="tab-1"
-          class="h-11 gap-2 data-[state=active]:after:bg-primary relative rounded-none px-0 py-2 after:absolute after:inset-x-0 after:bottom-0 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+          class="h-10 gap-2 data-[state=active]:after:bg-primary relative rounded-none px-0 py-2 after:absolute after:inset-x-0 after:bottom-0 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
         >
-          <span class="text-sm">Common</span>
+          <span class="text-[13px]">Common</span>
           <Badge
             class="size-4 text-[10px] transition-opacity group-data-[state=inactive]:bg-primary/15"
           >
@@ -34,15 +34,15 @@ import { PopoverContent } from '@renderer/shared/components/ui/popover'
         </TabsTrigger>
         <TabsTrigger
           value="tab-2"
-          class="h-11 data-[state=active]:after:bg-primary relative rounded-none px-0 py-2 after:absolute after:inset-x-0 after:bottom-0 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+          class="h-10 data-[state=active]:after:bg-primary relative rounded-none px-0 py-2 after:absolute after:inset-x-0 after:bottom-0 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
         >
-          <span class="text-sm">Group</span>
+          <span class="text-[13px]">Group</span>
         </TabsTrigger>
         <TabsTrigger
           value="tab-3"
-          class="h-11 data-[state=active]:after:bg-primary relative rounded-none px-0 py-2 after:absolute after:inset-x-0 after:bottom-0 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+          class="h-10 data-[state=active]:after:bg-primary relative rounded-none px-0 py-2 after:absolute after:inset-x-0 after:bottom-0 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
         >
-          <span class="text-sm">System</span>
+          <span class="text-[13px]">System</span>
         </TabsTrigger>
       </TabsList>
       <TabsContent value="tab-1">
