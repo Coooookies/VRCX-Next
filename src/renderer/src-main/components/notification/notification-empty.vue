d81e2401af4085af65f544c92b3514740cdb12ea
Coooookies <admin@mitay.net>
2025-08-29 02:56:47 +0800

    feat: Add notification of empty and loaded state components

---
+++ src/renderer/src-main/components/notification/notification-empty.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import NotificationEmptyIllustration from '@shared/assets/vector/notification-empty-illustration.svg?component'
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center w-full h-full">
+    <NotificationEmptyIllustration class="text-primary -mb-4" />
+    <div class="w-2/3 space-y-0.5 pb-12">
+      <p class="font-semibold text-center">暂时没有通知</p>
+      <p class="text-sm text-muted-foreground text-center">这里似乎非常安静</p>
+    </div>
+  </div>
+</template>
---
+++ src/renderer/src-main/components/notification/notification-loading.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Spinner } from '@renderer/shared/components/ui/spinner'
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center w-full h-full">
+    <Spinner class="size-12" />
+  </div>
+</template>
