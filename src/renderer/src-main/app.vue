a3a14dd20d356e5124c7491cf8e58b11ed369ae4
Coooookies <admin@mitay.net>
2025-08-29 02:57:57 +0800

    feat: Modification of the public time supply function

--- src/renderer/src-main/app.vue
+++ src/renderer/src-main/app.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { useLocaleUpdater } from '@renderer/shared/components/locale/locale-updater'
-import { useElapsedTimer } from '@renderer/shared/components/elapsed-timer/provider'
+import { useTimer } from '@renderer/shared/components/timer/provider'
 import { Toaster } from '@renderer/shared/components/ui/sonner'
 import Viewer from './pages/index.vue'
 
 useLocaleUpdater()
-useElapsedTimer()
+useTimer()
 </script>
 
 <template>
