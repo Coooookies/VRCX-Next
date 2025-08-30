790c74fdd94d667ced4c36392cb70f557e004e57
Coooookies <admin@mitay.net>
2025-08-31 02:18:25 +0800

    refactor: update badge-vrchat-plus component to use aspect ratio for better responsiveness

--- src/renderer/src-main/components/badge/badge-vrchat-plus.vue
+++ src/renderer/src-main/components/badge/badge-vrchat-plus.vue
@@ -3,7 +3,7 @@ import BadgeVRChatPlus from '@shared/assets/vector/badge-vrchat-plus.svg?compone
 </script>
 
 <template>
-  <div class="relative w-13 h-5.5 rounded bg-yellow-400">
+  <div class="relative w-13 aspect-[55/22] rounded bg-yellow-400">
     <BadgeVRChatPlus class="size-full text-stone-950" />
   </div>
 </template>
