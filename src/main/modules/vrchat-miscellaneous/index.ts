3d6506174cbef60509ea1ddeff973be78a18406c
Coooookies <admin@mitay.net>
2025-09-01 07:57:34 +0800

    feat: add VRChatWorkflowCoordinator dependency and bind events for server time fetching

--- src/main/modules/vrchat-miscellaneous/index.ts
+++ src/main/modules/vrchat-miscellaneous/index.ts
@@ -5,11 +5,13 @@ import { createLogger } from '@main/logger'
 import type { MobxState } from '../mobx-state'
 import type { VRChatAPI } from '../vrchat-api'
 import type { VRChatPipeline } from '../vrchat-pipeline'
+import type { VRChatWorkflowCoordinator } from '../vrchat-workflow-coordinator'
 
 export class VRChatMiscellaneous extends Module<{}> {
   @Dependency('MobxState') declare private mobx: MobxState
   @Dependency('VRChatAPI') declare private api: VRChatAPI
   @Dependency('VRChatPipeline') declare private pipeline: VRChatPipeline
+  @Dependency('VRChatWorkflowCoordinator') declare private workflow: VRChatWorkflowCoordinator
 
   private readonly logger = createLogger(this.moduleId)
   private repository!: MiscellaneousRepository
@@ -18,10 +20,17 @@ export class VRChatMiscellaneous extends Module<{}> {
   protected onInit(): void {
     this.repository = new MiscellaneousRepository(this.moduleId, this.mobx)
     this.fetcher = new MiscellaneousFetcher(this.logger, this.api, this.repository)
+    this.bindEvents()
   }
 
   protected onLoad(): void {
     this.fetcher.fetchServerTime()
+  }
+
+  private bindEvents(): void {
+    this.workflow.registerPostLoginTask('misc-fetcher', 20, async () => {
+      await this.fetcher.fetchServerTime()
+    })
   }
 
   public get Fetcher(): MiscellaneousFetcher {
