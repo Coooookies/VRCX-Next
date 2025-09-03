e9cbd24eef480542d6b0b731f0a254cb83570c40
Coooookies <admin@mitay.net>
2025-09-04 00:31:41 +0800

    feat: add image selection context component with props and template

---
+++ src/renderer/shared/components/image-selection-context.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ImageContext } from './ui/image'
+import type { ImageContextProps } from './ui/image'
+
+const props = withDefaults(
+  defineProps<
+    Omit<ImageContextProps, 'src'> & {
+      selectionId: string
+      size?: number
+    }
+  >(),
+  {
+    size: 64
+  }
+)
+const imageUrl = props.selectionId
+  ? `vrcxnext://image-selection-proxy/image/${props.selectionId}/${props.size}`
+  : ``
+</script>
+
+<template>
+  <ImageContext v-bind="props" :src="imageUrl" />
+</template>
