e8a4538828fe0e2ab787b80f1fbffa51811f3625
Coooookies <admin@mitay.net>
2025-09-04 00:32:14 +0800

    feat: add favorites page with image selection context and fallback handling

--- src/renderer/src-main/pages/favorites.vue
+++ src/renderer/src-main/pages/favorites.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
+import ImageSelectionContext from '@renderer/shared/components/image-selection-context.vue'
 import AppRoute from '../layouts/app-route.vue'
+import { ImageRoot, ImageFallback } from '@renderer/shared/components/ui/image'
 </script>
 
 <template>
   <AppRoute>
-    <p>2</p>
+    <ImageRoot class="block w-200">
+      <ImageSelectionContext
+        selection-id="imgsel_df5dbb9d-a21a-4d47-9e3f-5bf1e8fde8d9"
+        :size="200"
+        class="size-full object-cover"
+      />
+      <ImageFallback class="size-full flex items-center justify-center">
+        <p>none</p>
+      </ImageFallback>
+    </ImageRoot>
   </AppRoute>
 </template>
