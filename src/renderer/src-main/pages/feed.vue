1ada1ac00667f7fd124eaca8e6dda0a486e2dad2
Coooookies <admin@mitay.net>
2025-09-12 23:52:59 +0800

    feat: add current instance page and info tabs components

--- src/renderer/src-main/pages/feed.vue
+++ src/renderer/src-main/pages/feed.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import AppRoute from '../layouts/app-route.vue'
+import CurrentInstancePageTabs from '../components/current-instance/current-instance-page-tabs.vue'
 </script>
 
 <template>
   <AppRoute>
-    <p>3</p>
+    <div class="mx-auto py-10 w-[1038px]">
+      <CurrentInstancePageTabs />
+    </div>
   </AppRoute>
 </template>
