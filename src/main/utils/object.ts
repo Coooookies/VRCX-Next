ac6c545a468557307b7aad3701f04a2bdb120094
Coooookies <admin@mitay.net>
2025-09-24 21:09:00 +0800

    feat: refactor object diffing to return detailed diff results with before and after states

--- src/main/utils/object.ts
+++ src/main/utils/object.ts
@@ -1,11 +1,40 @@
-import { isEqual } from 'lodash'
+import { union, isEqual } from 'lodash'
 
-export function diffSurface<T extends object>(a: T, b: T): Partial<T> {
-  const diff: Partial<T> = {}
-  for (const key in b) {
-    if (!isEqual(a[key], b[key])) {
-      diff[key] = b[key]
+export interface DiffResult<T = unknown> {
+  keys: (keyof T)[]
+  diff: {
+    before: Partial<T>
+    after: Partial<T>
+  }
+}
+
+export function diffObjects<T extends object>(
+  objA: T,
+  objB: T,
+  whitelistKeys?: (keyof T)[]
+): DiffResult<T> {
+  const result: DiffResult<T> = {
+    keys: [],
+    diff: {
+      before: {},
+      after: {}
     }
   }
-  return diff
+
+  const keysToCompare =
+    whitelistKeys && whitelistKeys.length > 0
+      ? whitelistKeys.map((key) => String(key))
+      : union(Object.keys(objA), Object.keys(objB))
+
+  for (const key of keysToCompare) {
+    const valueA = objA[key]
+    const valueB = objB[key]
+
+    if (!isEqual(valueA, valueB)) {
+      result.diff.before[key as keyof T] = valueA
+      result.diff.after[key as keyof T] = valueB
+      result.keys.push(key as keyof T)
+    }
+  }
+  return result
 }
