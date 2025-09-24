b7db43ded153e96afb7b33f40e6711802d6ae103
Coooookies <admin@mitay.net>
2025-09-24 23:07:20 +0800

    feat: update CurrentInstanceEventHeader to accept joinedAt prop for dynamic display

--- src/renderer/src-main/components/current-instance/current-instance-event-header.vue
+++ src/renderer/src-main/components/current-instance/current-instance-event-header.vue
@@ -1,5 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ElapsedTimerText } from '@renderer/shared/components/timer'
+import { cn } from '@renderer/shared/utils/style'
+
+const props = defineProps<{
+  joinedAt: Date | null
+}>()
+</script>
 
 <template>
-  <div class="bg-muted/50 h-10 rounded-md"></div>
+  <div class="bg-muted/50 h-10 rounded-md pl-4 flex flex-row items-center">
+    <div class="flex flex-row items-center gap-2 text-xs">
+      <div
+        :class="
+          cn(
+            'relative size-3.5',
+            'after:absolute after:inset-[3px] after:rounded-full after:bg-primary',
+            'before:absolute before:inset-[3px] before:rounded-full before:bg-primary',
+            'before:animate-ping before:animation-duration-[1.5s]'
+          )
+        "
+      />
+      <ElapsedTimerText v-if="props.joinedAt" :start-time="props.joinedAt" />
+      <span v-else>{{ `--:--:--` }}</span>
+    </div>
+  </div>
 </template>
--- src/renderer/src-main/pages/current-instance.events.vue
+++ src/renderer/src-main/pages/current-instance.events.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import CurrentInstanceEventHeader from '../components/current-instance/current-instance-event-header.vue'
+import { useCurrentInstance } from '../composables/current-instance'
+
+const { instance } = useCurrentInstance()
 </script>
 
 <template>
   <div class="">
-    <CurrentInstanceEventHeader />
+    <CurrentInstanceEventHeader :joined-at="instance.currentInstance.locationJoinedAt" />
   </div>
 </template>
