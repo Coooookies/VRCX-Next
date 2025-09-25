c5ced6ce6ba3cd0c8e9e2e29ca3d586ea99b9b93
Coooookies <admin@mitay.net>
2025-09-25 21:40:54 +0800

    feat: update vrchat-instance ipc binding

--- src/renderer/shared/modules/vrchat-instances.ts
+++ src/renderer/shared/modules/vrchat-instances.ts
@@ -3,30 +3,27 @@ import { Dependency, Module } from '@shared/module-constructor'
 import type { IPCRenderer } from './ipc'
 import type { MobxRenderer } from './mobx-renderer'
 import type { InstanceSharedState } from '@shared/definition/mobx-shared'
-import type { InstanceUserActivity, InstanceUserSummary } from '@shared/definition/vrchat-instances'
+import type { InstanceEventMessage, InstanceUserSummary } from '@shared/definition/vrchat-instances'
 
 export class VRChatInstances extends Module {
   @Dependency('IPCRenderer') declare private ipc: IPCRenderer
   @Dependency('MobxRenderer') declare private mobx: MobxRenderer
 
   public readonly currentInstanceUsers = ref<InstanceUserSummary[]>([])
-  public readonly currentInstanceUserActivities = ref<InstanceUserActivity[]>([])
+  public readonly currentInstanceEvents = ref<InstanceEventMessage[]>([])
   private $!: InstanceSharedState
 
   protected async onInit() {
     this.bindEvents()
     this.$ = this.mobx.use<InstanceSharedState>(this.moduleId)
     this.currentInstanceUsers.value = await this.getCurrentInstanceUsers()
-    this.currentInstanceUserActivities.value = await this.getCurrentInstanceUserActivities()
+    this.currentInstanceEvents.value = await this.getCurrentInstanceEvents()
   }
 
   private bindEvents() {
-    this.ipc.listener.on(
-      'vrchat-instances:current-instance:append-user-activities',
-      (_, notifications) => {
-        this.currentInstanceUserActivities.value.push(...notifications)
-      }
-    )
+    this.ipc.listener.on('vrchat-instances:current-instance:append-events', (_, events) => {
+      this.currentInstanceEvents.value.push(...events)
+    })
 
     this.ipc.listener.on('vrchat-instances:current-instance:insert-users', (_, users) => {
       this.currentInstanceUsers.value.push(...users)
@@ -51,8 +48,8 @@ export class VRChatInstances extends Module {
       this.currentInstanceUsers.value = []
     })
 
-    this.ipc.listener.on('vrchat-instances:current-instance:clear-user-activities', () => {
-      this.currentInstanceUserActivities.value = []
+    this.ipc.listener.on('vrchat-instances:current-instance:clear-events', () => {
+      this.currentInstanceEvents.value = []
     })
   }
 
@@ -60,8 +57,8 @@ export class VRChatInstances extends Module {
     return this.ipc.emitter.invoke('vrchat-instances:get-current-instance-users')
   }
 
-  public getCurrentInstanceUserActivities() {
-    return this.ipc.emitter.invoke('vrchat-instances:get-current-instance-user-activities')
+  public getCurrentInstanceEvents() {
+    return this.ipc.emitter.invoke('vrchat-instances:get-current-instance-events')
   }
 
   public get state() {
