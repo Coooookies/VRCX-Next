861b3652fea9f6b57ff67612aa4bee97dd807b4b
Coooookies <admin@mitay.net>
2025-09-29 01:16:55 +0800

    chore: add string type for instanceName

--- src/main/modules/vrchat-instances/utils.ts
+++ src/main/modules/vrchat-instances/utils.ts
@@ -1,7 +1,11 @@
 import { v5 as uuidv5 } from 'uuid'
 import { INSTANCE_RECORD_NAMESPACE } from './constants'
 
-export function generateInstanceRecordId(worldId: string, instanceName, date: Date): string {
+export function generateInstanceRecordId(
+  worldId: string,
+  instanceName: string,
+  date: Date
+): string {
   const timestamp = date.getTime()
   const instanceKey = `${worldId}:${instanceName}:${timestamp}`
   return `visinst_${uuidv5(instanceKey, INSTANCE_RECORD_NAMESPACE)}`
