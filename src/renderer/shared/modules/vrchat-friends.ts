1f333b14db213198df56a596240bdf583f4af709
Coooookies <admin@mitay.net>
2025-10-02 02:25:11 +0800

    refactor: update friend session event handling and improve user state management

--- src/renderer/shared/modules/vrchat-friends.ts
+++ src/renderer/shared/modules/vrchat-friends.ts
@@ -15,37 +15,66 @@ export class VRChatFriends extends Module {
   protected async onInit() {
     this.bindEvents()
     this.$ = this.mobx.use<FriendSharedState>(this.moduleId)
-    this.friends.value = await this.getAllFriends()
+    this.friends.value = await this.getFriends()
   }
 
   private bindEvents() {
-    this.ipc.listener.on('vrchat-friends:friend:list-insert', (_, users) => {
-      this.friends.value.push(...users)
+    this.ipc.listener.on('vrchat-friends:friend-sessions:present', (_, friends) => {
+      this.friends.value.push(...friends)
     })
 
-    this.ipc.listener.on('vrchat-friends:friend:list-update', (_, users) => {
-      users.forEach((user) => {
-        const index = this.friends.value.findIndex((f) => f.userId === user.userId)
+    this.ipc.listener.on('vrchat-friends:friend-sessions:add', (_, friend) => {
+      this.friends.value.push(friend)
+    })
+
+    this.ipc.listener.on(
+      'vrchat-friends:friend-sessions:state',
+      (_, friendUserId, state, platform) => {
+        const index = this.friends.value.findIndex((f) => f.userId === friendUserId)
         if (index !== -1) {
-          this.friends.value[index] = user
+          this.friends.value[index] = {
+            ...this.friends.value[index],
+            state,
+            platform
+          }
         }
-      })
-    })
+      }
+    )
 
-    this.ipc.listener.on('vrchat-friends:friend:list-delete', (_, userId) => {
+    this.ipc.listener.on('vrchat-friends:friend-sessions:delete', (_, userId) => {
       this.friends.value.splice(
         this.friends.value.findIndex((f) => f.userId === userId),
         1
       )
     })
 
-    this.ipc.listener.on('vrchat-friends:friend:list-clear', () => {
+    this.ipc.listener.on('vrchat-friends:friend-sessions:update', (_, friendUserId, friend) => {
+      const index = this.friends.value.findIndex((f) => f.userId === friendUserId)
+      if (index !== -1) {
+        this.friends.value[index] = {
+          ...this.friends.value[index],
+          ...friend
+        }
+      }
+    })
+
+    this.ipc.listener.on('vrchat-friends:friend-sessions:location', (_, friendUserId, location) => {
+      const index = this.friends.value.findIndex((f) => f.userId === friendUserId)
+      if (index !== -1) {
+        this.friends.value[index] = {
+          ...this.friends.value[index],
+          location
+        }
+      }
+    })
+
+    this.ipc.listener.on('vrchat-friends:friend-sessions:clear', () => {
       this.friends.value = []
     })
   }
 
-  public getAllFriends() {
-    return this.ipc.emitter.invoke('vrchat-friends:get-friends')
+  public getFriends() {
+    return this.ipc.emitter.invoke('vrchat-friends:friend-sessions:get-friends')
   }
 
   public get state() {
--- src/renderer/shared/modules/vrchat-users.ts
+++ src/renderer/shared/modules/vrchat-users.ts
@@ -1,19 +1,19 @@
 import { Dependency, Module } from '@shared/module-constructor'
 import type { IPCRenderer } from './ipc'
 import type { MobxRenderer } from './mobx-renderer'
-import type { UserSharedState } from '@shared/definition/mobx-shared'
+import type { UserStoreSharedState } from '@shared/definition/mobx-shared'
 
 export class VRChatUsers extends Module {
   @Dependency('IPCRenderer') declare private ipc: IPCRenderer
   @Dependency('MobxRenderer') declare private mobx: MobxRenderer
 
-  private $!: UserSharedState
+  private $currentUser!: UserStoreSharedState
 
   protected async onInit() {
-    this.$ = this.mobx.use<UserSharedState>(this.moduleId)
+    this.$currentUser = this.mobx.use<UserStoreSharedState>('VRChatUsers:CurrentUserStore')
   }
 
-  public get state() {
-    return this.$
+  public get currentUser() {
+    return this.$currentUser
   }
 }
