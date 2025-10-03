40738ba00c478b0d13ead6969c536da4f6511681
Coooookies <admin@mitay.net>
2025-10-03 22:08:57 +0800

    feat: add getFile method to retrieve file details by fileId

--- src/main/api/vrchat/files.ts
+++ src/main/api/vrchat/files.ts
@@ -1,6 +1,6 @@
 import { attempt } from '@shared/utils/async'
 import type { Got, Response, HTTPError } from 'got'
-import type { ErrorResponse, FileAnalysis } from '@shared/definition/vrchat-api-response'
+import type { ErrorResponse, FileAnalysis, ModelFile } from '@shared/definition/vrchat-api-response'
 
 export class Files {
   constructor(private client: Got) {}
@@ -8,6 +8,14 @@ export class Files {
   public getFileVersionAnalysisSecurity(fileId: string, version: number) {
     return attempt<Response<FileAnalysis>, HTTPError<Response<ErrorResponse>>>(() =>
       this.client.get(`analysis/${fileId}/${version}/security`, {
+        responseType: 'json'
+      })
+    )
+  }
+
+  public getFile(fileId: string) {
+    return attempt<Response<ModelFile>, HTTPError<Response<ErrorResponse>>>(() =>
+      this.client.get(`file/${fileId}`, {
         responseType: 'json'
       })
     )
